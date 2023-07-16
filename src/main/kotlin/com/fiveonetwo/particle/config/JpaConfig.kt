c23f25b65f9967b8f7522473289a1ae08db0fa54
tianea2160 <hyunjun2087@gmail.com>
2023-07-16 15:08:43 +0900

    feat : create user, record entity

---
+++ src/main/kotlin/com/fiveonetwo/particle/config/JpaConfig.kt
@@ -0,0 +1,10 @@
+package com.fiveonetwo.particle.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EnableJpaRepositories
+@EnableJpaAuditing
+class JpaConfig
