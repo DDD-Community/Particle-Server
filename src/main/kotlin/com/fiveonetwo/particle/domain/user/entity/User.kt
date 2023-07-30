b6f0945fb93635e43ee56c38ec4f0d6cf31f0013
tianea2160 <hyunjun2087@gmail.com>
2023-07-30 21:05:25 +0900

    feat : create username random generator

--- src/main/kotlin/com/fiveonetwo/particle/domain/user/entity/User.kt
+++ src/main/kotlin/com/fiveonetwo/particle/domain/user/entity/User.kt
@@ -1,22 +1,61 @@
 package com.fiveonetwo.particle.domain.user.entity
 
 import com.fiveonetwo.particle.domain.common.entity.BaseTimeEntity
+import com.fiveonetwo.particle.domain.user.entity.UsernameContext.Companion.Color
+import com.fiveonetwo.particle.domain.user.entity.UsernameContext.Companion.Figure
 import com.fiveonetwo.particle.shared.utils.uuid
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 @Table(name = "users")
 @Entity
 class User(
-        @Id
-        @Column(name = "user_id", nullable = false)
-        val id: String = uuid(),
-        @Column(name = "nickname", nullable = false, columnDefinition = "text")
-        val nickname: String = "티클",
-        @Column(name = "provider", nullable = false)
-        val provider: String,
-        @Column(name = "identifier", nullable = false, columnDefinition = "text")
-        val identifier: String,
+    @Id
+    @Column(name = "user_id", nullable = false)
+    val id: String = uuid(),
+    @Column(name = "nickname", nullable = false, columnDefinition = "text")
+    val nickname: String = createRandomUsername(),
+    @Column(name = "provider", nullable = false)
+    val provider: String,
+    @Column(name = "identifier", nullable = false, columnDefinition = "text")
+    val identifier: String,
 ) : BaseTimeEntity()
+
+fun createRandomUsername(): String {
+    val colors = Color.values()
+    val figures = Figure.values()
+    return "${colors[Random.nextInt(0..colors.lastIndex)].value} ${figures[Random.nextInt(0..figures.lastIndex)].value}"
+}
+
+class UsernameContext {
+    companion object {
+        enum class Color(val value: String) {
+            RED("붉은"),
+            YELLOW("노란"),
+            ORANGE("오렌지"),
+            GREEN("초록"),
+            BLUE("파아란"),
+            PURPLE("보랏빛"),
+            PINK("핑크"),
+            MAGENTA("마젠타"),
+            CYAN("시안"),
+        }
+
+        enum class Figure(val value: String) {
+            ISOSCELES_TRIANGLE("이등변삼각형"),
+            RIGHT_TRIANGLE("직각삼각형"),
+            EQUILATERAL_TRIANGLE("정삼각형"),
+            SQUARE("정사각형"),
+            ELLIPSE("타원"),
+            CIRCLE("동그라미"),
+            PARALLELOGRAM("평행사변형"),
+            TRAPEZOID("사다리꼴"),
+            RECTANGLE("직사각형"),
+            RHOMBUS("마름모"),
+        }
+    }
+}
