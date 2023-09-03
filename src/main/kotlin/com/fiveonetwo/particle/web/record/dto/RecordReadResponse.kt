c3eed6f4ec093bcf68179a47e26519fbae201e25
tia <tia@techtaka.com>
2023-09-03 19:34:14 +0900

    Fix : record read response 누락된 필드 추가

--- src/main/kotlin/com/fiveonetwo/particle/web/record/dto/RecordReadResponse.kt
+++ src/main/kotlin/com/fiveonetwo/particle/web/record/dto/RecordReadResponse.kt
@@ -1,12 +1,15 @@
 package com.fiveonetwo.particle.web.record.dto
 
 import com.fiveonetwo.particle.domain.record.dto.RecordReadDTO
+import java.time.LocalDateTime
 
 class RecordReadResponse(
     val title: String,
     val url: String,
     val items: List<RecordItemCreateResponse>,
     val tags: List<String>,
+    val createdAt: LocalDateTime,
+    val createdBy: String,
 ) {
     companion object {
         fun from(dto: RecordReadDTO): RecordReadResponse {
@@ -19,7 +22,9 @@ class RecordReadResponse(
                         isMain = item.isMain
                     )
                 },
-                tags = dto.tags.map { tag -> tag.value }
+                tags = dto.tags.map { tag -> tag.value },
+                createdAt = dto.createdAt,
+                createdBy = dto.createdBy
             )
         }
     }
